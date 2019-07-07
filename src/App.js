1b154a8b2cc3dc83924bdfdd234228ddbbd0f0c4
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-07-07 13:59:19 -0700

    changing header accepting ideas

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <h1>New App idea</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           New App Coming Soon...
