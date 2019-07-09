f27591dda84d50273c2e36b3f382539faf59de24
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-07-09 15:51:55 -0700

    added in components

--- src/App.js
+++ src/App.js
@@ -6,9 +6,24 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>Gamer Ranks</h1>
+      <h1 style={{display:'inline-block'}}>GAMER RANKS</h1>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <div style={{height: '45vh'}}>
+      <h1 style={{display:'inline-block'}}>GAMER 1</h1>
+      </div>
+      <div style={{height: '70vh', backgroundColor: 'grey'}}>
+      <h1 style={{display:'inline-block'}}>GAMER 2</h1>
+      </div>
+      <div style={{height: '45vh', backgroundColor: 'green'}}>
+      <h1 style={{display:'inline-block'}}>GAMER 3</h1>
+      </div>
+      <div style={{height: '45vh'}}>
+      <h1 style={{display:'inline-block'}}>GAMER 4</h1>
+      </div>
+      <div style={{height: '70vh', backgroundColor: 'grey'}}>
+      <h1 style={{display:'inline-block'}}>GAMER 5</h1>
+      </div>
     </div>
   );
 }
