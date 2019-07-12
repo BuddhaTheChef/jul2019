13ce2277ace1d5100d6d28685bae982ab8b8d0fa
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-07-11 17:31:32 -0700

    added in more animations for parallax scrolling

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
-import Skyline from '../assets/skyline1.png'
-import XboxController from '../assets/xboxcontroller.png'
+import Skyline from '../assets/skyline1.png';
+import XboxController from '../assets/xboxcontroller.png';
+import PS4Controller from '../assets/ps4controller1.png';
+import Keyboard from '../assets/mechkeyboard.png'
 import {Parallax} from 'react-scroll-parallax';
 
 class LandingPage extends Component {
@@ -11,16 +13,22 @@ class LandingPage extends Component {
                 <Parallax id="logo" y={[-100, 70]} tagOuter="figure"><h1 style={{textAlign: 'center', fontSize: '121px'}}>GAME TIME MACHINE</h1></Parallax>
                 <div id="mainImg"><img src={Skyline} alt="skyline" style={{width: '100%'}}/></div>
             </div> 
-            <Parallax id="xbox-div" y={[105, 300]} x={[-150, 50]} style={{overflow: 'hidden'}}>
+            <Parallax id="xbox-div" y={[120, 300]} x={[-135, 50]} style={{overflow: 'hidden'}}>
             <img src={XboxController} alt="xboxcontroller" style={{height: '260px'}}/>
             </Parallax>
             <div style={{ marginTop: '470px', height: '45vh'}}>
+            <Parallax id="xbox-div" y={[125, 280]} x={[200, 0]} style={{overflow: 'hidden'}}>
+            <img src={PS4Controller} alt="ps4controller" style={{height: '260px'}}/>
+            </Parallax>
       <h1 style={{display:'inline-block', margin: '0 45%', width: 'max-content'}}>GAMER 1</h1>
       </div>
       <div style={{height: '70vh', backgroundColor: 'grey'}}>
+            <Parallax id="xbox-div" y={[10, 250]} x={[-290, 10]} style={{overflow: 'hidden'}}>
+            <img src={Keyboard} alt="Keyboard" style={{height: '200px'}}/>
+            </Parallax>
       <h1 style={{display:'inline-block'}}>GAMER 2</h1>
       </div>
-      <div style={{height: '45vh', backgroundColor: 'green'}}>
+      <div style={{height: '45vh', background: 'linear-gradient(#66FFFD, #FFF)'}}>
       <h1 style={{display:'inline-block'}}>GAMER 3</h1>
       </div>
       <div style={{height: '45vh'}}>
