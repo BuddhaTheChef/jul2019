c830097528a31dc4d5a2377b53b2e282ffc74742
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-08-19 13:55:20 -0700

    upding users state and projects

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -8,6 +8,7 @@ const ProjectList = ({projects}) => {
                         <div key={project.id}>
                             <h1 style={{color:'#09e6bc', display: 'inline-flex'}}>{project.title} &nbsp; - &nbsp; <span><h1 style={{fontSize: 18, fontStyle: 'oblique'}}>({project.gamerTag})</h1></span></h1>
                             <h2 style={{color:'whitesmoke', font: 'italic',fontWeight: 200, fontStyle: 'oblique'}}>{project.content}</h2>
+                            <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
                         </div>
                     )
                 })}
