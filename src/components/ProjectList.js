228be70acf2355ce0bbe2bedba3953a7d4acd73f
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-08-23 17:12:50 -0700

    added in styles to profile page

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,12 +6,13 @@ import { connect } from 'react-redux'
 const ProjectList = ({projects,auth}) => {
     console.log(projects)
     console.log(auth)
+    // const alternatingColor = ['#d5d5d5', '#a9a9a9'];
         return (
             <div>
-                {projects && projects.map(project => {
+                {projects && projects.map((project, index) => {
                     return (
-                        <Link to={{ pathname: '/project/:id', state: {project}}} style={{textDecoration: 'none'}}>
-                        <div key={project.id} style={{backgroundColor: 'slategray', padding: '30px', margin: '135px 0px', borderRadius: '10px', width: '900px',boxShadow:'1px 6px 12px 2px rgba(0,0,0,0.51)'}}>
+                        <Link to={{ pathname: '/project/:id', state: {project}}} style={{textDecoration: 'none'}} className='bigLink'>
+                        <div key={project.id} className='proj-list'>
                             <h1 style={{color:'#09e6bc', display: 'inline-flex'}}>{project.title} &nbsp; - &nbsp; <span><h1 style={{fontSize: 18, fontStyle: 'oblique'}}>({project.gamerTag})</h1></span></h1>
                             <h2 style={{color:'whitesmoke', font: 'italic',fontWeight: 200, fontStyle: 'oblique'}}>{project.content}</h2>
                             <p style={{color: 'black'}}>Posted by {project.authorFirstName} {project.authorLastName} on <span>{moment(project.createdAt.toDate()).calendar()}</span></p> 
--- src/components/ProjectSummary.js
+++ src/components/ProjectSummary.js
@@ -8,8 +8,9 @@ class ProjectSummary extends Component {
         return (
             <div style={{marginTop: '160px'}}>
             {project ?
-                 <div key={project.id} style={{backgroundColor: 'slategray', padding: '30px', margin: '35px 0px', borderRadius: '10px'}}>
-                            <h1 style={{color:'#09e6bc', display: 'inline-flex'}}>{project.title} &nbsp; - &nbsp; <span><h1 style={{fontSize: 18, fontStyle: 'oblique'}}>({project.gamerTag})</h1></span></h1>
+                 <div key={project.id} style={{backgroundColor: 'slategray', padding: '30px', margin: '35px 0px', borderRadius: '10px', height: '800px', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
+                            <h1 style={{color:'#09e6bc', display: 'inline-flex'}}>{project.title}</h1>
+                            <h1 style={{fontSize: 68, fontStyle: 'oblique', color:'#09e6bc'}}>({project.gamerTag})</h1>
                             <h2 style={{color:'whitesmoke', font: 'italic',fontWeight: 200, fontStyle: 'oblique'}}>{project.content}</h2>
                             <p>Posted by {project.authorFirstName} {project.authorLastName} on <span>{moment(project.createdAt.toDate()).calendar()}</span></p> 
                             <p>Created on: {moment(project.dateCreated).calendar()}</p>
