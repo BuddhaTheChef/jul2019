5c7ad3552be9b5717f47bcf1b2c348d99eb2f09b
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-09-03 15:30:54 -0700

    added in game stats api

--- src/components/UpcomingTitles.js
+++ src/components/UpcomingTitles.js
@@ -1,24 +1,39 @@
 import React, { Component } from "react";
-import Footer from "./Footer";
+import axios from 'axios';
 
 class UpcomingTitles extends Component {
+  state = {
+    teams: []
+  }
+
+  componentDidMount() {
+    const API_KEY = process.env.REACT_APP_GAME_SOURCE_API;
+    axios.get(`https://api.pandascore.co/teams?token=${API_KEY}`)
+      .then(res => {
+        console.log(res)
+        const teams = res.data;
+        this.setState({ teams });
+      })
+  }
   render() {
     return (
       <div>
         <div
           style={{
-            marginTop: "60px",
+            marginTop: "140px",
             height: "-webkit-fill-available",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center"
           }}
         >
-          <h1>Upcoming Titles</h1>
+        <div>
+        { this.state.teams.map(team => 
+        <div style={{width: '400px', height: 'inherit', background: 'slategrey', justifyContent:'space-around', display: 'flex', flexDirection: 'column', margin: '50px'}}>
+        <img src={team.image_url} alt='No Team Logo'  style={{height: '300px'}}/>
+        Team Name: {team.name}
+        Plays: {team.current_videogame.name}
+        Players: {team.players.map(player => <div>{player.name}</div> )}
+        </div>)}
         </div>
-        <footer className="mainFooterDiv">
-          <Footer/>
-        </footer>
+        </div>
       </div>
     );
   }
