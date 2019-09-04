c9458bc3f758afd9b81e9f3529ea941a87a9fb3c
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-09-04 16:25:10 -0700

    added in styling for game teams

--- src/components/UpcomingTitles.js
+++ src/components/UpcomingTitles.js
@@ -26,14 +26,16 @@ class UpcomingTitles extends Component {
         >
         <div style={{display: 'inline-flex', flexWrap: 'wrap', justifyContent: 'center'}}>
         { this.state.teams.map(team => 
-        <div style={{width: '400px', height: 'inherit', background: 'slategrey', justifyContent:'space-around', display: 'inline-flex', flexDirection: 'column', margin: '50px', borderRadius: '10px'}}>
+        <div style={{width: '400px', height: 'inherit', background: 'slategrey', justifyContent:'space-around', display: 'inline-flex', flexDirection: 'column', margin: '50px', borderRadius: '10px', padding: '12px'}}>
         <img src={team.image_url} alt='No Team Logo'  style={{height: '300px'}}/>
-        <div style={{background: '#09e6bc', color: 'whitesmoke'}}>
-        Team Name: {team.name}
+        <div style={{background: '#09e6bc', color: 'whitesmoke', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
+        <h1 style={{color: 'black', marginBottom: 0}}>{team.name}</h1>
         <br/>
-        Video Game: {team.current_videogame.name}
+         <h3 style={{margin: 0}}>Team for <span style={{color: 'blue'}}>{team.current_videogame.name}</span></h3>
         <br/>
-        Players: {team.players.map(player => <div>{player.name}</div> )}
+        <div style={{display:'inline-flex',color: 'black', fontWeight: 'bold', flexWrap: 'wrap', justifyContent: 'center'}}>
+        {team.players.map(player => <div style={{display: 'inline-flex', width: 'inherit', margin: '9px 14px'}}>{player.name}</div> )}
+        </div>
         </div>
         </div>)}
         </div>
