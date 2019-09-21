0a2d1ddb79edbe0adebcb28fcd361fc949c77c11
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-09-20 18:10:12 -0700

    added in pc steps

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -7,16 +7,190 @@ class FriendsList extends Component {
       <div>
         <div
           style={{
-            marginTop: "60px",
+            marginTop: "140px",
             height: "-webkit-fill-available",
             display: "flex",
-            justifyContent: "center",
-            alignItems: "center"
+            flexDirection: 'column'
           }}
         >
           <h1>Friends list or building components for building a computer step by step and all the parts</h1>
+          <div>
+          <h1>Picking the right parts</h1>
+            <p>
+Intel Motherboard
+The most important part to get right is picking parts that do what you want and work with each other. Choosing wrong or incompatible parts may cause issues, damage to other components or require time for returning said products to retailers. In short, we'll need a CPU, GPU, RAM, motherboard, and some storage to complete the barebones checklist.
+
+Looking inside a PC case can reveal a mess of electronics to someone who doesn't know what does what. Fear not, as we've got you covered when it comes to picking the right parts. Check out our best guides for each component and then check for compatibilities using our brief checklist below.
+
+Products used in this guide
+Best cases
+Best CPU (best CPU coolers)
+Best PSU
+Best motherboards
+Best RAM
+Best GPU
+Best GPU for gamers
+Best SSD
+Compatability
+GPUs
+Here's what you're looking for with components:
+
+Case: Cases come in different form factors, depending on the size of PC you wish to build. Less important overall.
+PSU: Needs to supply enough stable power (500W is usually a good value to start with).
+CPU: You have the choice of AMD and Intel for desktop processors. Take a look at our best guide and note the sockets they use as you'll need to match the socket for the motherboard.
+Motherboard: A motherboard simply needs to match the same socket type of the CPU. Chipset and other features are all down to pricing and preference.
+RAM: Faster frequencies and lower latencies generally mean better the RAM, though you'll need to make sure your motherboard can support the same clock speed or it'll be limited. Most new boards support DDR4.
+Storage: Entirely down to personal requirements, though we always recommend an SSD for installing Windows.
+GPU: This is optional and is only really needed if you plan to do some gaming or intensive workloads. In that case, spend as much as you can.
+But wait! Even after double and triple checking that everything will place nicely together, it's recommended you use an automated tool that checks entered components against a database to confirm that there are no conflicts. It's worth firing up PC Part Picker, entering in all your components and then checking everything over.
+</p>
+<h1>Tools You Need</h1>
+<p>
+The bare essentials you'll need to build a PC including the following:
+
+Toolkit.
+LED light source.
+Flat workspace (no carpet).
+Anti-static mat.
+Anti-static wristband.
+Magnetic parts tray.
+Free workspace (never use carpet).
+Anti-static Wristband
+</p>
+
+<h1>Installing Components</h1>
+<p>
+Installing the PSU
+Installing the CPU
+Installing the RAM
+Installing a motherboard
+Installing storage drives
+Plugging everything in
+Adding case fans
+Installing a GPU
+Avoid these rookie PC building mistakes
+</p>
+
+<h1>Installing the PSU</h1>
+<p>
+Slide the PSU into the mounting area with the fan facing up or down.
+Screw in bundled screws (should come with the case) in the four holes at the rear of the case.
+Ensure the unit is switched off using the rear switch.
+Plug the PSU into a power socket to ground the PC.
+Bind all the power cables coming out of the PSU together and put them to one side.
+PSU
+Depending on the PSU you've purchased, it may be one of the following:
+
+Fully modular: No cables are permanently connected to the PSU.
+Semi-modular: Some cables are permanently fixed, usually ATX and CPU power.
+Non-modular: Every cable is connected and cannot be removed from the PSU.
+Fully and semi-modular PSUs are easiest to work with as you'll have an easier time making sure all cables look neat and tidy.
+</p>
+
+<h1>Installing the CPU</h1>
+<p>
+We can now insert the CPU into the motherboard. This is a super-easy process, regardless as to whether you're rocking an AMD or Intel chip.
+
+Unpack the motherboard from its packaging.
+Place the motherboard atop its cardboard box. (It's safe here. Never place on carpet.)
+Open the CPU latch.
+Remove the plastic cap.
+Insert the CPU, matching notches and indicators to the socket. (Do not apply pressure, it should lie flush without force.)
+Close the latch to secure the CPU. (This step will require some amount of pressure.)
+Intel CPU Paste
+Now we can use the thermal paste and attach the CPU cooler to complete everything processor-related. Depending on the cooler you've purchased, you may have a layer of thermal paste already applied. If not, we'll need to do this before installing the cooler. (Tip: less is more.)
+</p>
+
+<h1>Installing the RAM</h1>
+<p>
+RAM
+RAM is a sensitive component and as such should be handled with care. It's recommended that contact is avoided with the contact pins on the underside of each module. The RAM slots on a motherboard are located to the right of the CPU but do check the motherboard manual as to which RAM slots are to be used, depending on how many sticks you have. A motherboard with four slots or more, you'll want to populate slots one and two first.
+
+Unclip the RAM locks on the motherboard module slots.
+Match the RAM module notches to those found in the slots on the motherboard.
+Insert the module carefully, using light even force across the stick.
+
+RAM
+After it inserts into the slot, push down on either side of the RAM module until it securely clicks into place.
+How to install RAM
+</p>
+
+<h1>Installing the moterboard</h1>
+<p>
+With the CPU, the cooler, and RAM modules installed, the motherboard is ready to be screwed to the back plate inside the case. First, we need to install the I/O shield, a long piece of metal that has cutouts for the all the rear ports and connections. This is optional but recommended accessory helps provide electromagnetic interference (EMI) protection.
+
+Place the I/O shield to the rear cutout in the PC case and click into place. (This can be super-fiddly.)
+Hover the motherboard atop the back plate to see where standoffs need to be screwed in. Different sizes need different holes.
+
+Standoffs are the screws that are installed on the motherboard backplate, which then allows the motherboard to rest atop them and for screws to secure the board. Some cases have them pre-installed.
+
+Install the standoffs for the motherboard, if not already pre-installed. (Some backplates may feature ATX labels next to holes to guide you.)
+Lower the motherboard onto the standoffs.
+
+Motherboard
+Tighten the screws, but do not overtighten them — just enough to secure the board in place.
+Check the sides of the motherboard by lightly pulling to make sure every inch has been secured.
+Motherboard
+MSI Performance Gaming X470GPLUS
+Ryzen up again.
+</p>
+
+<h1>Installing storage drives</h1>
+<p>
+SSD
+Storage drives are important devices that hold not only the operating system but also all programs, personal media, and all other data. It's recommended to use an SSD (2.5-inch) drive for the main OS partition, which enables for quick booting and solid performance. Data can be stored on traditional (3.5-inch) mechanical drives.
+
+Depending on the case and how drive bays are configured, 2.5- and 3.5-inch drives may be able to be installed at the same points or using dedicated brackets. Be sure to double check the PC case manual. We'll go over how your PC case may have both configured below.
+
+2.5-inch SSD
+Take the SSD bracket and attach the drive to it, lining up the screw holes.
+Screw in the drive using appropriate screws.
+Attach the bracket and secure it to the case.
+3.5-inch HDD
+Extract the HDD bracket from one of the bays.
+Slide the mechanical drive into the bracket.
+Secure the drive to said bracket using screws or screwless mechanism.
+Slide the bracket and drive back into the bay.
+SSD
+</p>
+
+<h1>Plug everything in</h1>
+<p>
+With all major components now installed, it's time to hook everything up to the power supply and motherboard. Generally speaking, it's best to start with the power to the motherboard itself.
+
+Motherboard: The PSU has two cables for the board: 24-pin ATX and 8-pin CPU. Route these behind the backplate, using available grommets and cutouts.
+Storage drives: Most SSDs and HDDs today use SATA connectors. These are 15-pin flat connectors with a dip on one side to prevent plugging them in the wrong way. Route these from the PSU to the drive bays and mounts, using more than one lead from the PSU if required.
+PSU Cable
+Next, we need to connect the storage drives to the motherboard, using SATA data cables. These are essentially smaller versions of their power siblings that carry data instead. The motherboard usually has SATA ports located to the lower-left hand-side. It doesn't matter which ports are used. Much like the power cables, route these through cut-outs and grommets to the drive bays and mounts.
+
+We now have power to our storage drives and to the motherboard, as well as data channels between drives and the main board. All that's really left is to connect front panel I/O and other optional connectors (front panel USB, HD audio, etc.). Connecting the front panel I/O, which includes the power switch, reset switch, HDD activity LED, and power LED can be a tricky step.
+
+These pins stand up from the motherboard, which requires a careful approach when plugging in the small connectors. The motherboard manual should provide information as to the layout of these pins and what they correspond to - though usually the below layout is used:
+
+Motherboard I/O
+The front USB 3.0 and HD audio pins can be located on the motherboard. Again, refer to the manual for exact placement as not all motherboards are created equal.
+
+
+Adding case fans
+For added cooling capacity (and room for overclocking or more stable operation), throwing in an extra fan or two is worthwhile. PC cases may come with fans pre-installed, but if not it's simple to screw them into fan mounts. Just be sure to align them correctly and have the blades pointing the correct way. Fans can have small arrows on the side that show which way the blades spin and where airflow will be directed.
+</p>
+
+<h1>Installing a GPU</h1>
+<p>
+GPU
+Need some extra power for gaming and intense workloads? A GPU is a must-have. As an added bonus, it's really easy to install a graphics card. We usually leave the GPU until last because of how much space they usually take within the chassis.
+
+Unscrew the rear PCI case brackets that align with the PCI slot you will use.
+Check that the motherboard GPU card latch is open.
+Remove the protective cap that covers the PCIe strip on the GPU.
+Line up the GPU to the PCIe slot on the motherboard.
+Carefully insert the card and push down once it has made contact with the slot, listening out for a click of the latch securing.
+Use the rear bracket screws to secure the GPU to the PC case for added stability.
+If the GPU requires additional PSU power, connect the necessary cables.
+</p>
+          </div>
         </div>
-        <footer className="mainFooterDiv">
+        <footer className="mainFooterDiv" style={{marginTop: '1100px'}}>
           <Footer />
         </footer>
       </div>
