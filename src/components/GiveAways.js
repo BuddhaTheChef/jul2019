44dcff3c6ecf11cccb6087658276f6e1a7573353
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-11-20 13:59:02 -0800

    added in new photos

--- src/components/GiveAways.js
+++ src/components/GiveAways.js
@@ -2,6 +2,14 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
 import Footer from "./Footer";
+import nt1 from '../assets/newTech1.jpg';
+import nt2 from '../assets/newTech2.jpg';
+import nt3 from '../assets/newTech3.jpg';
+import nt4 from '../assets/newTech4.jpg';
+import nt5 from '../assets/newTech5.jpg';
+import nt6 from '../assets/newTech6.jpg';
+import nt7 from '../assets/newTech7.jpg';
+import nt8 from '../assets/newTech8.jpg';
 
 class GiveAways extends Component {
   render() {
@@ -23,9 +31,9 @@ class GiveAways extends Component {
           <h1 style={{color: 'black', paddingTop: '60px', fontSize: '5rem'}}>New In Tech</h1>
           <div style={{background: 'whitesmoke', width: '100%'}}>
           <div class="main-tech-top-div">
-            <div class="tech-top-divs">Hi</div>
-            <div class="tech-top-divs">Bye</div>
-            <div class="tech-top-divs">Cry</div>
+            <div class="tech-top-divs"><img src={nt1} alt="tech-pic" className="newTechPics"/></div>
+            <div class="tech-top-divs"><img src={nt2} alt="tech-pic" className="newTechPics"/></div>
+            <div class="tech-top-divs"><img src={nt3} alt="tech-pic" className="newTechPics"/></div>
           </div>
           <div class="main-tech-mid-div">
             Other content
