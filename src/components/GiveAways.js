dc550103764cbf6316ccd0a370d2f0cd439ab6d1
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-11-22 15:45:19 -0800

    added in content

--- src/components/GiveAways.js
+++ src/components/GiveAways.js
@@ -30,35 +30,43 @@ class GiveAways extends Component {
             paddingBottom: '70px'
           }}
         >
-          <h1 style={{color: 'black', paddingTop: '60px', fontSize: '5rem'}}>New In Tech</h1>
-          <div style={{background: 'whitesmoke', width: '100%'}}>
+          <h1 style={{ color: 'black', paddingTop: '60px', fontSize: '5rem' }}>New In Tech</h1>
+          <div style={{ background: 'whitesmoke', width: '100%' }}>
+            <div class="main-tech-top-div">
+              <div class="tech-top-divs"><img src={nt1} alt="tech-pic" className="newTechPics" /></div>
+              <div class="tech-top-divs"><img src={nt2} alt="tech-pic" className="newTechPics" /></div>
+              <div class="tech-top-divs"><img src={nt3} alt="tech-pic" className="newTechPics" /></div>
+            </div>
+            <div class="main-tech-mid-div">
+            <div class="main-tech-inner-div">
+              <h3 style={{ margin: '0px' }}>TOP TECH</h3>
+              <p style={{ margin: '0px' }}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            </div>
+            </div>
+          </div>
           <div class="main-tech-top-div">
-            <div class="tech-top-divs"><img src={nt1} alt="tech-pic" className="newTechPics"/></div>
-            <div class="tech-top-divs"><img src={nt2} alt="tech-pic" className="newTechPics"/></div>
-            <div class="tech-top-divs"><img src={nt3} alt="tech-pic" className="newTechPics"/></div>
-          </div>
-          <div class="main-tech-mid-div">
-            Other content
-          </div>
-          </div>
-          <div class="main-tech-top-div">
-            <div class="tech-top-divs"><img src={nt4} alt="tech-pic" className="newTechPics"/></div>
-            <div class="tech-top-divs"><img src={nt5} alt="tech-pic" className="newTechPics"/></div>
-            <div class="tech-top-divs"><img src={nt6} alt="tech-pic" className="newTechPics"/></div>
+            <div class="tech-top-divs"><img src={nt4} alt="tech-pic" className="newTechPics" /></div>
+            <div class="tech-top-divs"><img src={nt5} alt="tech-pic" className="newTechPics" /></div>
+            <div class="tech-top-divs"><img src={nt6} alt="tech-pic" className="newTechPics" /></div>
           </div>
           <div class="main-tech-mid-div ">
             Other content
           </div>
           <div class="main-tech-top-div">
-            <div class="tech-top-divs"><img src={nt7} alt="tech-pic" className="newTechPics"/></div>
-            <div class="tech-top-divs"><img src={nt8} alt="tech-pic" className="newTechPics"/></div>
-            <div class="tech-top-divs"><img src={nt9} alt="tech-pic" className="newTechPics"/></div>
+            <div class="tech-top-divs"><img src={nt7} alt="tech-pic" className="newTechPics" /></div>
+            <div class="tech-top-divs"><img src={nt8} alt="tech-pic" className="newTechPics" /></div>
+            <div class="tech-top-divs"><img src={nt9} alt="tech-pic" className="newTechPics" /></div>
           </div>
           <div class="main-tech-mid-div">
             Other content
           </div>
           <div class="main-tech-top-div">
-            <div class="tech-top-divs"><img src={nt10} alt="tech-pic" className="newTechPics"/></div>
+            <div class="tech-top-divs"><img src={nt10} alt="tech-pic" className="newTechPics" /></div>
             <div class="tech-top-divs">Bye</div>
             <div class="tech-top-divs">Cry</div>
           </div>
@@ -74,7 +82,7 @@ class GiveAways extends Component {
           </div>
         </div>
         <footer className="mainFooterDiv">
-          <Footer/>
+          <Footer />
         </footer>
       </div>
     );
